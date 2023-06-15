86050dbcd9d3fc50ac4a82cf4b49b0bfe96d2418
lambda-san7 <codekiwi132@gmail.com>
2023-06-14 17:07:51 -0700

    this shit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DVD-Logo
 DVD logo
+
+import pygame
+import os
+
+window = pygame.display.set_mode((500,500))
+
+x = 100
+y = 0
+
+running = True
+
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            running = False
+    x += 1
+
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+
+    pygame.display.quit()
+
+    window = pygame.display.set_mode((501,500))
+    pygame.display.update()
+pygame.quit()
---
+++ main.py
@@ -0,0 +1,116 @@
+import pygame
+import random
+import time
+
+pygame.init()
+width, height = 800, 600
+window = pygame.display.set_mode((width, height),pygame.RESIZABLE)
+
+
+dvd_image = pygame.transform.scale(pygame.image.load("dvd.png"),(185,85))
+
+color = (50,50,50)
+
+corners = 0
+flipped_x = False
+flipped_y = False
+
+class logo:
+    def __init__(self,speed,function):
+        self.x = random.randint(1,200)
+        self.y = random.randint(1,200)
+        self.w = 185
+        self.h = 85
+        self.horizontal = speed
+        self.vertical = speed
+        self.function = function
+    def handle(self):
+        global corners
+        global color
+        if self.function == "obs":
+            color = (0,255,0)
+        flipped_x, flipped_y = False, False
+        self.x += self.horizontal
+        self.y += self.vertical
+        if self.x + self.horizontal <= 0:
+            self.horizontal += self.horizontal * -2
+            flipped_x = True
+        if self.y + self.vertical <= 0:
+            self.vertical += self.vertical * -2
+            flipped_y = True
+        if (self.x + self.horizontal) + self.w >= pygame.display.Info().current_w:
+            self.horizontal -= self.horizontal * 2
+            flipped_x = True
+        if (self.y + self.vertical) + self.h >= pygame.display.Info().current_h:
+            self.vertical -= self.vertical * 2
+            flipped_y = True
+        if flipped_x and flipped_y:
+            window.fill((0,255,0))
+            corners += 1
+            if self.function == "teleport":
+                self.x, self.y = random.randint(1,200),random.randint(1,200)
+            if self.function == "speed":
+                self.horizontal += 10
+                self.vertical += 10
+            if self.function == "chaos":  
+                self.x, self.y = random.randint(0,pygame.display.Info().current_w),random.randint(0,pygame.display.Info().current_w)
+                self.horizontal = random.randint(1,1000)
+                self.vertical = random.randint(1,1000)
+                color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+                print(self.horizontal, self.vertical)
+        window.blit(dvd_image,(self.x,self.y))
+
+dvd = logo(50,"chaos")
+
+class text:
+    def __init__(self, size, text):
+        self.font = pygame.font.Font("font.fon",size)
+        self.text_holder = text
+        self.text = self.font.render(text, True, (255,255,255))
+    def render(self,x,y):
+        font = pygame.font.Font("font.fon",32)
+        text = font.render(self.text_holder, True, (0,0,0))
+        window.blit(text,(x + 2,y))
+        window.blit(text,(x - 2,y))
+        window.blit(text,(x,y + 2))
+
+        window.blit(text,(x,y - 2))
+
+        window.blit(text,(x + 2,y + 2))
+        window.blit(text,(x - 2,y + 2))
+        window.blit(text,(x - 2,y - 2))
+        window.blit(text,(x + 2,y - 2))
+
+        window.blit(self.text,(x,y))
+
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("DVD Bounce")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                dvd2 = logo(dvd.horizontal,dvd.function)
+                dvd = dvd2
+            if event.key == pygame.K_BACKSPACE:
+                dvd2 = logo(50,dvd.function)
+                dvd = dvd2
+            if event.key == pygame.K_ESCAPE:
+                pygame.display.set_caption("Exiting...")
+                window.fill(0,0,0)
+                corner = text(32, "Thanks For Using DVD Bounce!")
+                corner.render((pygame.display.Info().current_w / 2) - corner.text.get_height(),(pygame.display.Info().current_h / 2) - corner.text.get_width())
+                pygame.display.update()
+                running = False
+    clock.tick(60)
+    window.fill(color)
+    corner = text(32, str(corners))
+    corner.render(10,10)
+    dvd.handle()
+    pygame.display.update()
+
+pygame.quit()
